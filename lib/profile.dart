089edfc38ce3a2fb0481e6395061b60632ca504f
MD SHAHRIAR PERVEZ EFTI <cse049.shahriarefti@gmail.com>
2024-04-30 23:48:31 +0600

    initial commit

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,59 +2,72 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class profile extends StatefulWidget {
-  const profile({super.key});
+  const profile({Key? key});
 
   @override
-  State<profile> createState() => _profileState();
+  State<profile> createState() => _ProfileState();
 }
 
-class _profileState extends State<profile> {
+class _ProfileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    final mediaQuery = MediaQuery.of(context);
+    final screenWidth = mediaQuery.size.width;
 
+    final bool isLargeScreen = screenWidth > 600;
+
+    return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Profile"
-            ,style: TextStyle(
-                fontSize: 16,
+            Text(
+              "Profile",
+              style: TextStyle(
+                fontSize: isLargeScreen ? 24 : 16,
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
-              ),),
-            SizedBox(height: 16),
-            CircleAvatar(
-              radius: 50,
-              backgroundImage: NetworkImage('https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+              ),
             ),
             SizedBox(height: 16),
             Center(
-              child: Text(
-                'Matilda Brown',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundImage: NetworkImage(
+                    'https://images.pexels.com/photos/1391499/pexels-photo-1391499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Matilda Brown',
+              style: TextStyle(
+                fontSize: isLargeScreen ? 32 : 24,
+                fontWeight: FontWeight.bold,
               ),
             ),
             SizedBox(height: 8),
             Text(
               'matildabrown@gmail.com',
               style: TextStyle(
-                fontSize: 16,
+                fontSize: isLargeScreen ? 18 : 16,
                 color: Colors.grey,
               ),
             ),
             SizedBox(height: 8),
-            Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
-
-
+            Expanded(
+              child: SingleChildScrollView(
+                child: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                  style: TextStyle(
+                    fontSize: isLargeScreen ? 18 : 16,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
-
   }
 }
